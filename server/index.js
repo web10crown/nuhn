330ff62d2a14d80338c1ec87132181a09a44dd95
Neeraj Rana <99588906+web10crown@users.noreply.github.com>
2023-10-23 15:04:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,13 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const checkCreditCard = require('./nuhn');
+
 app.use(cors());
 app.use(express.json());
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
 
 app.post('/', async (req, res) => {
     var cardRes =  checkCreditCard(req.body.number);
